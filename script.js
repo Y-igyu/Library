feb6a5c62ee203f8c8befd70108420f385fafa37
Y-igyu <145227350+Y-igyu@users.noreply.github.com>
2024-04-11 17:41:14 -0500

    Added form and function to take info

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-const myLibrary = [];
+let myLibrary = [];
 
-function Book(name) {
-  this.name = name;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(newBook) {
-  myLibrary.push(new Book(newBook))
+function render() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = '';
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement('div');
+    bookEl.innerHTML = `<p>${book.title}</p>`;
+    libraryEl.appendChild(bookEl);
+  }
 }
 
-addBookToLibrary('Harry Potter');
-addBookToLibrary('Divergent');
+function addBookToLibrary() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  render();
+}
+
+let newBookBtn = document.querySelector("#newBook")
+newBookBtn.addEventListener("click", function() {
+  let newBookForm = document.querySelector("#new-book-form")
+  newBookForm.style.display = "block";
+})
+
+document.querySelector("#new-book-form").addEventListener('submit', function(event) {
+  event.preventDefault();
+  addBookToLibrary();
+})
