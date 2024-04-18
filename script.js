aff7dcd9cea00e12fa2229b08d825968622b789b
Y-igyu <145227350+Y-igyu@users.noreply.github.com>
2024-04-18 12:45:41 -0500

    Input value in dialog push to myLibrary

--- script.js
+++ script.js
@@ -22,6 +22,41 @@ newBook.addEventListener("click", () => {
   dialog.showModal();
 });
 
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const page = document.querySelector("#page");
+const read = document.querySelector("#read");
+const close = document.querySelector("#close");
+
 addBook.addEventListener("click", () => {
+  if (valueCheck() == 1) {
+    return alert("Fill it out if you want to add it loser");
+  }
+  myLibrary.push(new Book(title.value, author.value, page.value, read.checked));
+  title.value = "";
+  author.value = "";
+  page.value = "";
+  read.checked = false;
   dialog.close();
 });
+
+close.addEventListener("click", () => {
+  title.value = "";
+  author.value = "";
+  page.value = "";
+  read.checked = false;
+  dialog.close();
+});
+
+function valueCheck() {
+  if (title.value == "") {
+    return 1;
+  }
+  if (author.value == "") {
+    return 1;
+  }
+  if (page.value == "") {
+    return 1;
+  }
+  return 0;
+}
