6ac1450afe2da0b418e3bd93ea16d0ead58c2f69
Y-igyu <145227350+Y-igyu@users.noreply.github.com>
2024-04-18 18:46:52 -0500

    Changing js

--- script.js
+++ script.js
@@ -10,29 +10,42 @@ class Book {
   }
 }
 
-function addBookToLibrary() {
-  // stuff
-}
+const close = document.querySelector("#close");
+const del = document.querySelector(".delete");
 
 const dialog = document.querySelector("#bookInfo");
 const newBook = document.querySelector("#newBook");
 const addBook = document.querySelector("#addBook");
 
+const library = document.querySelector("#library");
+
+function addBookToLibrary() {
+  // do stuff
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+  let newBook = new Book(title, author, pages, read);
+
+  library.innerHTML = `<div class="book">
+    <p class="title">${newBook.title}</p>
+    <p class="author">By: ${newBook.author}</p>
+    <p class="pages">${newBook.pages}pgs</p>
+    <p class="read">Read: <input type="checkbox" ${newBook.read} ></p>
+    <button class="delete">Delete</button>
+  </div>`;
+
+  console.log(newBook);
+}
+
 newBook.addEventListener("click", () => {
   dialog.showModal();
 });
-
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const page = document.querySelector("#page");
-const read = document.querySelector("#read");
-const close = document.querySelector("#close");
 
 addBook.addEventListener("click", () => {
   if (valueCheck() == 1) {
     return alert("Fill it out if you want to add it loser");
   }
-  myLibrary.push(new Book(title.value, author.value, page.value, read.checked));
   title.value = "";
   author.value = "";
   page.value = "";
@@ -40,6 +53,7 @@ addBook.addEventListener("click", () => {
   dialog.close();
 });
 
+// close dialog box
 close.addEventListener("click", () => {
   title.value = "";
   author.value = "";
@@ -48,6 +62,12 @@ close.addEventListener("click", () => {
   dialog.close();
 });
 
+// delete book from library
+del.addEventListener("click", () => {
+  console.log("delete");
+});
+
+// checks if dialog input is empty
 function valueCheck() {
   if (title.value == "") {
     return 1;
