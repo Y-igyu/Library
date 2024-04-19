a693774cf0b36a357331b3fb40145a6dc8720937
Y-igyu <145227350+Y-igyu@users.noreply.github.com>
2024-04-19 12:42:14 -0500

    Book that is added displays in html

--- script.js
+++ script.js
@@ -17,48 +17,70 @@ const dialog = document.querySelector("#bookInfo");
 const newBook = document.querySelector("#newBook");
 const addBook = document.querySelector("#addBook");
 
-const library = document.querySelector("#library");
+function render() {
+  let libraryEl = document.querySelector("#library");
+  libraryEl.innerHTML = "";
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let bookEl = document.createElement("div");
+    bookEl.className = "book";
+
+    // checkbox in read input is checked or not
+    if (book.read == true) {
+      bookEl.innerHTML = `<p class="title">${book.title}</p>
+        <p class="author">By: ${book.author}</p>
+        <p class="page">${book.page}</p>
+        <p class="read">Read: <input type="checkbox" checked/></p>`;
+    } else {
+      bookEl.innerHTML = `<p class="title">${book.title}</p>
+        <p class="author">By: ${book.author}</p>
+        <p class="page">${book.page}</p>
+        <p class="read">Read: <input type="checkbox"/></p>`;
+    }
+
+    libraryEl.appendChild(bookEl);
+  }
+}
 
 function addBookToLibrary() {
   // do stuff
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
-  let pages = document.querySelector("#pages").value;
+  let page = document.querySelector("#page").value;
   let read = document.querySelector("#read").checked;
-  let newBook = new Book(title, author, pages, read);
 
-  library.innerHTML = `<div class="book">
-    <p class="title">${newBook.title}</p>
-    <p class="author">By: ${newBook.author}</p>
-    <p class="pages">${newBook.pages}pgs</p>
-    <p class="read">Read: <input type="checkbox" ${newBook.read} ></p>
-    <button class="delete">Delete</button>
-  </div>`;
+  let newBook = new Book(title, author, page, read);
 
-  console.log(newBook);
+  myLibrary.push(newBook);
+
+  render();
 }
 
+// shows dialog box
 newBook.addEventListener("click", () => {
   dialog.showModal();
 });
 
+// add book to library
 addBook.addEventListener("click", () => {
   if (valueCheck() == 1) {
     return alert("Fill it out if you want to add it loser");
   }
-  title.value = "";
-  author.value = "";
-  page.value = "";
-  read.checked = false;
+  addBookToLibrary();
+
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#page").value = "";
+  document.querySelector("#read").checked = false;
   dialog.close();
 });
 
 // close dialog box
 close.addEventListener("click", () => {
-  title.value = "";
-  author.value = "";
-  page.value = "";
-  read.checked = false;
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#page").value = "";
+  document.querySelector("#read").checked = false;
   dialog.close();
 });
 
@@ -69,13 +91,13 @@ del.addEventListener("click", () => {
 
 // checks if dialog input is empty
 function valueCheck() {
-  if (title.value == "") {
+  if (document.querySelector("#title").value == "") {
     return 1;
   }
-  if (author.value == "") {
+  if (document.querySelector("#author").value == "") {
     return 1;
   }
-  if (page.value == "") {
+  if (document.querySelector("#page").value == "") {
     return 1;
   }
   return 0;
